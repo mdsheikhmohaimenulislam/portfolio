55fe20d46bb267988c3f078bc12328380a2e5887
Mohai menul islam <mdsheikhmohaimenulislam@gmail.com>
2025-06-29 14:34:48 +0600

    Footer section done

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,121 +1,121 @@
-// import React from "react";
-// import { BsFacebook } from "react-icons/bs";
-// import { FaGithub, FaWhatsapp } from "react-icons/fa6";
-// import { Link } from "react-scroll";
+import React from "react";
+import { BsFacebook } from "react-icons/bs";
+import { FaGithub, FaWhatsapp } from "react-icons/fa6";
+import { Link } from "react-scroll";
 
-// const Footer = () => {
-//   return (
-//     <footer className="footer mt-40 footer-horizontal footer-center bg-base-200 text-base-content rounded p-10">
-//       <div>
-//         <div className="text-2xl mb-2 font-bold">
-//           <span className="text-blue-500">&lt;</span> 
-//           Mohaimenul
-//           <span className="text-blue-500">/</span>
-//           islam
-//           <span className="text-blue-500">&gt;</span>
-//         </div>
-// <div className="flex mb-3 gap-2 items-center justify-center md:justify-start">
-//   <a
-//     className="cursor-pointer"
-//     href="https://github.com/mdsheikhmohaimenulislam"
-//     target="_blank"
-//     rel="noopener noreferrer"
-//   >
-//     <FaGithub size={30} />
-//   </a>
-//   <a
-//     className="text-green-500 cursor-pointer"
-//     href="https://wa.me/8801715755177"
-//     target="_blank"
-//     rel="noopener noreferrer"
-//   >
-//     <FaWhatsapp size={30} />
-//   </a>
-//   <a
-//     className="text-blue-500 cursor-pointer"
-//     href="https://www.facebook.com/mohymimislam"
-//     target="_blank"
-//     rel="noopener noreferrer"
-//   >
-//     <BsFacebook size={30} />
-//   </a>
-// </div>
-//         <ul className="flex gap-5">
-//           <li>
-//             <Link
-//               to="home"
-//               smooth={true}
-//               duration={500}
-//               offset={-80}
-//               spy={true}
-//               activeClass="text-blue-500 font-extrabold border-b-2 border-blue-500"
-//               className="text-xl font-extrabold cursor-pointer"
-//             >
-//               Home
-//             </Link>
-//           </li>
-//           <li>
-//             <Link
-//               to="project"
-//               smooth={true}
-//               duration={500}
-//               offset={-80}
-//               spy={true}
-//               activeClass="text-blue-500 font-extrabold border-b-2 border-blue-500"
-//               className="text-xl font-extrabold cursor-pointer"
-//             >
-//               Project
-//             </Link>
-//           </li>
-//           <li>
-//             <Link
-//               to="skills"
-//               smooth={true}
-//               duration={500}
-//               offset={-80}
-//               spy={true}
-//               activeClass="text-blue-500 font-extrabold border-b-2 border-blue-500"
-//               className="text-xl font-extrabold cursor-pointer"
-//             >
-//               Skills
-//             </Link>
-//           </li>
-//           <li>
-//             <Link
-//               to="about"
-//               smooth={true}
-//               duration={500}
-//               offset={-80}
-//               spy={true}
-//               activeClass="text-blue-500 font-extrabold border-b-2 border-blue-500"
-//               className="text-xl font-extrabold cursor-pointer"
-//             >
-//               About
-//             </Link>
-//           </li>
-//           <li>
-//             <Link
-//               to="contact"
-//               smooth={true}
-//               duration={500}
-//               offset={-80}
-//               spy={true}
-//               activeClass="text-blue-500 font-extrabold border-b-2 border-blue-500"
-//               className="text-xl font-extrabold cursor-pointer"
-//             >
-//               Contact
-//             </Link>
-//           </li>
-//         </ul>
-//       </div>
-//       <aside>
-//         <p>
-//           Copyright © {new Date().getFullYear()} - All rights reserved by{" "}
-//           <span className="text-blue-400">Mohaimenul islam</span>
-//         </p>
-//       </aside>
-//     </footer>
-//   );
-// };
+const Footer = () => {
+  return (
+    <footer className="footer mt-40 footer-horizontal footer-center bg-base-200 text-base-content rounded p-10">
+      <div>
+        <div className="text-2xl mb-2 font-bold">
+          <span className="text-blue-500">&lt;</span> 
+          Mohaimenul
+          <span className="text-blue-500">/</span>
+          islam
+          <span className="text-blue-500">&gt;</span>
+        </div>
+<div className="flex mb-3 gap-2 items-center justify-center md:justify-start">
+  <a
+    className="cursor-pointer"
+    href="https://github.com/mdsheikhmohaimenulislam"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    <FaGithub size={30} />
+  </a>
+  <a
+    className="text-green-500 cursor-pointer"
+    href="https://wa.me/8801715755177"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    <FaWhatsapp size={30} />
+  </a>
+  <a
+    className="text-blue-500 cursor-pointer"
+    href="https://www.facebook.com/mohymimislam"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    <BsFacebook size={30} />
+  </a>
+</div>
+        <ul className="flex gap-5">
+          <li>
+            <Link
+              to="home"
+              smooth={true}
+              duration={500}
+              offset={-80}
+              spy={true}
+              activeClass="text-blue-500 font-extrabold border-b-2 border-blue-500"
+              className="text-xl font-extrabold cursor-pointer"
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="project"
+              smooth={true}
+              duration={500}
+              offset={-80}
+              spy={true}
+              activeClass="text-blue-500 font-extrabold border-b-2 border-blue-500"
+              className="text-xl font-extrabold cursor-pointer"
+            >
+              Project
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="skills"
+              smooth={true}
+              duration={500}
+              offset={-80}
+              spy={true}
+              activeClass="text-blue-500 font-extrabold border-b-2 border-blue-500"
+              className="text-xl font-extrabold cursor-pointer"
+            >
+              Skills
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="about"
+              smooth={true}
+              duration={500}
+              offset={-80}
+              spy={true}
+              activeClass="text-blue-500 font-extrabold border-b-2 border-blue-500"
+              className="text-xl font-extrabold cursor-pointer"
+            >
+              About
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="contact"
+              smooth={true}
+              duration={500}
+              offset={-80}
+              spy={true}
+              activeClass="text-blue-500 font-extrabold border-b-2 border-blue-500"
+              className="text-xl font-extrabold cursor-pointer"
+            >
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <aside>
+        <p>
+          Copyright © {new Date().getFullYear()} - All rights reserved by{" "}
+          <span className="text-blue-400">Mohaimenul islam</span>
+        </p>
+      </aside>
+    </footer>
+  );
+};
 
-// export default Footer;
+export default Footer;
