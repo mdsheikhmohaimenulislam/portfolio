9ffddaa6fa4766238498a6b54718853feae5f176
Mohai menul islam <mdsheikhmohaimenulislam@gmail.com>
2025-07-14 11:09:46 +0600

    resume upload done

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Banner from "../Components/Banner/Banner";
 import SkillsSection from "../Components/SkillsSection/SkillsSection";
-import Projects from "../Components/Projects/Projects";
+
 import Contact from "../Components/Contact/Contact";
 import AboutSection from "../Components/AboutSection/AboutSection";
+import Projects from "../Components/Projects/Projects";
 
 const Home = () => {
   return (
