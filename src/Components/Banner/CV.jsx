f863cc92538b53cf4f3bee8170868d80c9a37493
Luffy <mdsheikhmohaimenulislam@gmail.com>
2025-12-03 22:44:31 +0600

    added new resume

--- src/Components/Banner/CV.jsx
+++ src/Components/Banner/CV.jsx
@@ -4,7 +4,7 @@ export default function CV() {
   return (
     <div>
       <a
-        href="../../../public/MohaimenulIslam res(Front-end Developer).pdf"
+        href="./MohaimenulIslam res(Front-end Developer).pdf"
         download
         className="inline-block text-sm sm:text-base bg-blue-500 text-white font-bold rounded-2xl px-4 py-2 hover:bg-blue-600 transition"
       >
